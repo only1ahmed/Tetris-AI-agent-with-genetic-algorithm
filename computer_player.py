55d04c9655a500044e87f607a26a28c79bd18b82
ahmed wesam <awessam127@gmail.com>
2024-05-02 16:13:13 +0300

    base code

---
+++ computer_player.py
@@ -0,0 +1,73 @@
+import tetris_base
+
+
+# IMPORTANT NOTES:
+# The board contains two type of characters (.) and (O) "without brackets"
+# (.) means that the cell is BLANK "it is declared as BLANK globaly, so use it"
+# (O) means that the cell is occupied
+# Each cell has its own color
+
+
+# Main function for making the computer play
+def computer_plays(board, chromosom):
+    pass
+
+
+# Refer to the function (calc_move_info)
+# It should return the optimal rotation and column for the piece
+def test_all_moves(board, piece, chromosom):
+    pass
+
+
+# Play the move after finding the optimal one
+# Refer to the function (run_game)
+def play_move(board, rotation, column):
+    pass
+
+
+# for each hole, count how many blocks are above it and add it to the total that you will return from the function.
+def count_hole_effect(board):
+    pass
+
+
+# For each column, if there is a hole *segment*, count how many block needs to be cleared for the entire segment to be free (have nothing above it)
+def count_hole_segements_effect(board):
+    pass
+
+
+# How many rows are cleared in the move
+# give some n*X points in case of n row cleared (choose X, but don't choose is negative nor too big)
+def rows_cleared_effect(current_board, previous_board):
+    pass
+
+
+# max(Max height - 8, 0) so simple, right?
+def height_effect():
+    pass
+
+
+# empty columns - 1, even more simple!
+def columns_effect():
+    pass
+
+
+# NOTE: MAKE SURE TO INCREASE THE REWARD FOR MORE CONSECUTIVE ROWS
+
+# Reward for each *1* consecutive row with 1 column (the same column) only empty.
+def one_rows_effect():
+    pass
+
+
+# Reward for each *2* consecutive row with 2 column (the same column) only empty.
+def two_rows_effect():
+    pass
+
+
+# Reward for each *3* consecutive row with 3 column (the same column) only empty.
+def three_rows_effect():
+    pass
+
+
+# Reward for each *4* consecutive row with 4 column (the same column) only empty.
+def four_rows_effect():
+    pass
---
+++ model.py
@@ -0,0 +1,54 @@
+import tetris_base
+import computer_player
+
+NUM_OF_GENES = 9
+COUNT_OF_CHROMOSOMES = 15
+NUM_OF_EVOLUTIONS = 10
+PERECENTAGE_OF_MUTATED = 0.2
+CHROMOSOMS = []
+
+
+class Chromosom:
+    def __init__(self, genes=None):
+        self.Genes = []
+        if genes == None:
+            for i in rage(NUM_OF_GENES):
+                self.Genes.append(random.randint(-1000, 1000))
+        else:
+            self.Genes = genes
+
+
+# NOTE: maybe try to apply some sort of dynamic inputing to the global chromosoms (not nessacary)
+def init_chromosoms(mode=None):
+    for i in range(COUNT_OF_CHROMOSOMES):
+        CHROMOSOMS.append(Chromosom())
+
+
+# The main function for training the chromosomes
+def train():
+    pass
+
+
+# Apply ecolution on chromosomes
+def evolute():
+    pass
+
+
+# Play the game and evaluate the final score of the given chromosom (The highest final score the better)
+def evaluate(chromosom):
+    pass
+
+
+# Select the best suited chromosomes for the next stage of mating
+def selection(scores):
+    pass
+
+
+# Mate chromosomes to get the new generation
+def crossover():
+    pass
+
+
+# Mutate some of the chromosomes of the new generation
+def mutate():
+    pass
