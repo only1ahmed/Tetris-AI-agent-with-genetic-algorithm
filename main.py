ad359efffe12353537e0aafea30fee3f4f0a07c5
Error message: AMFAM <abdulrahmanmfam2003@gmail.com>
2024-05-07 02:09:44 +0300

    Revert "Fawzy is falah"
    This reverts commit afd635d1a2191f056a6bc6db5cbc0ba7cb5e5d13.

--- main.py
+++
@@ -1,6 +0,0 @@
-import tetris_base as tb
-import model
-NUM_OF_GENERATIONS = 10
-genetic_algorithm = model.GeneticAlgorithm(NUM_OF_GENERATIONS)
-genetic_algorithm.init_chromosomes()
-genetic_algorithm.train()
