e85e070f2cbc7abfe75df8892d6f07e9a06a2fb9
Error message: AMFAM <abdulrahmanmfam2003@gmail.com>
2024-05-09 19:45:43 +0300

    Abo Abdo is tired

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tetris_base as tb
 import numpy as np
 import model
 
+# IF YOU WANT TO TRAIN 
 # GA = model.GeneticAlgorithm(num_of_generations=2,
 #                             max_score= 1000,
 #                             calculate_next_move=False,
@@ -10,7 +11,7 @@ import model
 # GA.train()
 # GA.save_history("training_history.csv")
 
-
+# IF YOU WANT TO PLAY WITH A SINGLE CHROMOSOME
 optimal_genes = [-6.03021361728919,1.842716403806107,-4.994838657280255,-2.581971055440853,-4.740969302492218]
 
 chrom = model.Chromosome(heuristic_names=['aggregate_height','complete_lines', 'holes', 'bumpiness','four_rows'],genes=optimal_genes,calculate_next_move=False)
