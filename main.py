0dee0d8c1d6498e4a83e656138baea60b3b3577c
Error message: AMFAM <abdulrahmanmfam2003@gmail.com>
2024-05-12 12:47:43 +0300

    I don't know what i changed

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import model
 
 # IF YOU WANT TO TRAIN 
 # GA = model.GeneticAlgorithm(num_of_generations=2,
-#                             max_score= 1000,
-#                             calculate_next_move=False,
+#                             max_score=10000,
+#                             calculate_next_move=True,
+#                             speed=30,
+
 #                             heuristic_names=['aggregate_height','complete_lines', 'holes', 'bumpiness','four_rows'])
 # GA.load_chromosomes("saved_chromosome.csv")
 # GA.train()
@@ -14,12 +16,15 @@ import model
 # IF YOU WANT TO PLAY WITH A SINGLE CHROMOSOME
 optimal_genes = [-6.03021361728919,1.842716403806107,-4.994838657280255,-2.581971055440853,-4.740969302492218]
 
-chrom = model.Chromosome(heuristic_names=['aggregate_height','complete_lines', 'holes', 'bumpiness','four_rows'],genes=optimal_genes,calculate_next_move=False)
+chrom = model.Chromosome(heuristic_names=['aggregate_height','complete_lines', 'holes', 'bumpiness','four_rows'],
+                         genes=optimal_genes,
+                         calculate_next_move=False)
+
 game_score = tb.run_game_ai(is_training=False,
                             chromosome=chrom,
-                            max_score=1000,
+                            max_score=10000,
                             is_score_limited=True,
                             is_piece_limited=False,
-                            speed= 1000)
+                            speed= 10)
 
 chrom.save_history("chromosome_history.csv")
